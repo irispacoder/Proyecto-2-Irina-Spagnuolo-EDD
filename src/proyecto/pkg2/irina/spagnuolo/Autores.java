c72d8c971e8cea279448ba941920a0c81431e554
irispacoder <irinaspa@correo.unimet.edu.ve>
2025-11-23 04:28:44 -0400

    se agrego la clase autores

---
+++ src/proyecto/pkg2/irina/spagnuolo/Autores.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.pkg2.irina.spagnuolo;
+
+/**
+ *
+ * 
+ */
+public class Autores {
+    private String nombreAutor;
+    private int altura;
+    private Autores hijoIzquierdo;
+    private Autores hijoDerecho;
+    private Lista<Resumen> resumenes;
+
+    public Autores(String nombreAutor, Resumen resumen) {
+        this.nombreAutor = nombreAutor;
+        this.altura = 1;
+        this.hijoIzquierdo = null;
+        this.hijoDerecho = null;
+        this.resumenes = new Lista<>();
+        this.resumenes.addFinal(resumen);
+    }
+
+    /**
+     * @return the nombreAutor
+     */
+    public String getNombreAutor() {
+        return nombreAutor;
+    }
+
+    /**
+     * @param nombreAutor the nombreAutor to set
+     */
+    public void setNombreAutor(String nombreAutor) {
+        this.nombreAutor = nombreAutor;
+    }
+
+    /**
+     * @return the altura
+     */
+    public int getAltura() {
+        return altura;
+    }
+
+    /**
+     * @param altura the altura to set
+     */
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    /**
+     * @return the hijoIzquierdo
+     */
+    public Autores getHijoIzquierdo() {
+        return hijoIzquierdo;
+    }
+
+    /**
+     * @param hijoIzquierdo the hijoIzquierdo to set
+     */
+    public void setHijoIzquierdo(Autores hijoIzquierdo) {
+        this.hijoIzquierdo = hijoIzquierdo;
+    }
+
+    /**
+     * @return the hijoDerecho
+     */
+    public Autores getHijoDerecho() {
+        return hijoDerecho;
+    }
+
+    /**
+     * @param hijoDerecho the hijoDerecho to set
+     */
+    public void setHijoDerecho(Autores hijoDerecho) {
+        this.hijoDerecho = hijoDerecho;
+    }
+
+    /**
+     * @return the resumenes
+     */
+    public Lista<Resumen> getResumenes() {
+        return resumenes;
+    }
+
+    /**
+     * @param resumenes the resumenes to set
+     */
+    public void setResumenes(Lista<Resumen> resumenes) {
+        this.resumenes = resumenes;
+    }
+    
+    
+    public void addResumen(Resumen resumen){
+        if (!resumenes.searchElem(resumen)) {
+            resumenes.addFinal(resumen);
+        }
+    }
+    
+}
